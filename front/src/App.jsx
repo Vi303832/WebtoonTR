a671c3fea18e986a26595fd5b7cf3e1d3a26576b
Akif <mehmetakiftanyerii@hotmail.com>
2025-03-03 20:39:40 +0300

    Proje ve navbar oluşturuldu

---
+++ front/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+import './App.css'
+import cat from "./assets/cutecat.jpg"
+
+function App() {
+
+
+
+
+  return (
+    <>
+      <div>
+        <div className='min-h-screen'>
+          <Navbar />
+
+
+
+
+        </div>
+
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ front/src/Navbar.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import Logo from "./assets/WebtoonTRLogo.png"
+import { FaSearch } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { LuPen } from "react-icons/lu";
+
+function Navbar() {
+    return (
+        <div>
+
+
+            <div className='h-[100px]  w-full  font-Inter text-black text-lg bg-[#E44539] flex items-center justify-center '>
+                <div className='h-full w-[80%] flex justify-between  items-center gap-0  '>
+                    <div className='flex items-center justify-between w-full'>
+                        <div className='flex items-center  gap-10 text-white'>
+                            <img src={Logo} className='w-[124px] h-[124px] relative top-2' />
+                            <div className='flex gap-10'>
+                                <div className='h-[32px]'>
+                                    KATMAN
+                                </div>
+                                <div className='h-[32px]'>
+                                    YILDIZLAR
+                                </div>
+                                <div className='h-[32px]'>
+                                    SEZON
+                                </div>
+                                <div className='h-[32px]'>
+                                    POPÜLER SERİLER
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className='flex items-center gap-5 '>
+
+                            <button className='h-[36px] w-[84px] flex gap-1 items-center rounded-xl text-white bg-[#4CAADF]  '>
+                                <LuPen className=' ml-2  text-xl ' />
+                                <div className='text-xs font-light '>
+                                    Yayınla!
+
+                                </div>
+                            </button>
+                            <button className='h-[32px] w-[84px] border-2 border-white rounded-xl text-center text-sm text-white font-light'>
+                                Giriş Yap
+                            </button>
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+
+
+        </div>
+
+    )
+}
+
+export default Navbar
